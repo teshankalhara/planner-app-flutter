7396fd4d26438e896890a792fb8e7b26b6493f5a
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-07 02:03:07 +0530

    refactor: remove AppBar from main widget to streamline UI

--- lib/main.dart
+++ lib/main.dart
@@ -51,10 +51,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.grey,
