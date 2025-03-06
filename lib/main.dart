a3778e92e25c959ca96ca90d4f30110512b4848d
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-07 02:14:36 +0530

    feat: replace MyHomePage with MainScreen for improved navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:planner_app/pages/add_page/add_page.dart';
-import 'package:planner_app/pages/favourite_page/favourite_page.dart';
-import 'package:planner_app/pages/home_page/home_page.dart';
-import 'package:planner_app/pages/profile_page/profile_page.dart';
+import 'package:planner_app/pages/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,69 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Planner App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List<Widget> _pages = [
-    const HomePage(),
-    const AddPage(),
-    const FavouritePage(),
-    const ProfilePage(),
-  ];
-
-  int _selectedIndex = 0;
-
-  void setNavigator(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        elevation: 1,
-        currentIndex: _selectedIndex,
-        onTap: (int index) => setNavigator(index),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, size: 30),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add, size: 30),
-            label: 'Add',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite, size: 30),
-            label: 'Favourite',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person, size: 30),
-            label: 'Profile',
-          ),
-        ],
-      ),
-      body: _pages[_selectedIndex],
+      home: MainScreen(),
     );
   }
 }
