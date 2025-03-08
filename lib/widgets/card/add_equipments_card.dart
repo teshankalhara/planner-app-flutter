d30241f2ec35ee263e41660de2742b0c8d5d81a2
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-09 04:36:53 +0530

    feat: add AddEquipmentsCard widget for managing equipment display

---
+++ lib/widgets/card/add_equipments_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddEquipmentsCard extends StatefulWidget {
+  const AddEquipmentsCard({super.key});
+
+  @override
+  State<AddEquipmentsCard> createState() => _AddEquipmentsCardState();
+}
+
+class _AddEquipmentsCardState extends State<AddEquipmentsCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
