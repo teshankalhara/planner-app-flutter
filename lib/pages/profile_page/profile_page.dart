6f35757b7ef3354a72792352e3c62723eac34b5b
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-09 23:27:21 +0530

    feat: enhance ProfilePage with user progress and exercise/equipment lists

--- lib/pages/profile_page/profile_page.dart
+++ lib/pages/profile_page/profile_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:planner_app/constants/colors.dart';
+import 'package:planner_app/constants/consant_values.dart';
+import 'package:planner_app/data/user_data.dart';
+import 'package:planner_app/widgets/card/profile_exercise_card.dart';
+import 'package:planner_app/widgets/card/progress_card.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -8,8 +14,149 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final DateFormat formatter = DateFormat('EEEE, MMMM');
+  final DateFormat dayFormatter = DateFormat('dd');
+  final userData = user;
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: const Text("Profile Page"));
+    DateTime now = DateTime.now();
+    String formattedDate = formatter.format(now);
+    final progressValue = userData.calculateTotalCaloriesBurned() / 2;
+    // ignore: avoid_print
+    print(progressValue);
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(kDefaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  formattedDate.toUpperCase(),
+                  style: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    color: kSubtitleColor,
+                  ),
+                ),
+                Text(
+                  "Hello, ${userData.fullName}",
+                  style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700,
+                    color: kMainColor,
+                  ),
+                ),
+                const SizedBox(height: 15),
+                const Text(
+                  "Your Progress",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w800),
+                ),
+                const SizedBox(height: 15),
+                ProgressCard(progressValue: progressValue, total: 100),
+                const SizedBox(height: 15),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    // ignore: deprecated_member_use
+                    color: kSubtitleColor.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Total Minutes Spend: ${userData.calculateTotalMinutesSpent()}",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800,
+                          color: kGradientBottomColor,
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      Text(
+                        "Totla Exercises Completed: ${userData.totalExercisedCompleted}",
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        "Totla Equipments handovered: ${userData.totalEquipmentHandOvered}",
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 15),
+                const Text(
+                  "Your Exercises",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w800,
+                    color: kGradientBottomColor,
+                  ),
+                ),
+                const SizedBox(height: 15),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: userData.exerciseList.length,
+                  itemBuilder: (context, index) {
+                    return ProfileExerciseCard(
+                      exerciseName: userData.exerciseList[index].exerciseName,
+                      image: userData.exerciseList[index].exerciseImgUrl,
+                      markAsDone: () {
+                        setState(() {
+                          userData.markExerciseAsCompleted(
+                            userData.exerciseList[index].id,
+                          );
+                        });
+                      },
+                    );
+                  },
+                ),
+                const SizedBox(height: 15),
+                const Text(
+                  "Your Equipments",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w800,
+                    color: kGradientBottomColor,
+                  ),
+                ),
+                const SizedBox(height: 15),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: userData.equipmentList.length,
+                  itemBuilder: (context, index) {
+                    return ProfileExerciseCard(
+                      exerciseName: userData.equipmentList[index].equipmentName,
+                      image: userData.equipmentList[index].equipmentImgUrl,
+                      markAsDone: () {
+                        setState(() {
+                          userData.markEquipmentAsHandedOver(
+                            userData.equipmentList[index].id,
+                          );
+                        });
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
